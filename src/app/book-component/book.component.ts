6fdea5d3896ac3aba11c5fb08be3322a2e3ab64f
harumaru77 <qkdvo!042577R>
2017-06-02 11:14:14 +0900

    init

---
+++ src/app/book-component/book.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-book',
+  templateUrl: './book.component.html',
+})
+export class BookComponent implements OnInit {
+  booksResult: any = [];
+  books = [
+    {id: '001', name: '타이젠 모바일 프로그래밍', price: 31500, date: '20150115', img: 'img001.jpg'},
+    {id: '002', name: '아파치 코르도바', price: 31500, date: '20150131', img: 'img002.jpg'},
+    {id: '003', name: '웹앱 하이브리드 앱', price: 31500, date: '2015320', img: 'img003.jpg'},
+    {id: '004', name: 'OpenGL ES 2.0 안드로이드 게임프로그래밍', price: 31500, date: '20150601', img: 'img004.jpg'},
+    {id: '005', name: 'Ext JS6 프로그래밍', price: 31500, date: '20151005', img: 'img005.jpg'},
+  ];
+  ngOnInit() {
+  }
+  constructor() {
+    for(let book of this.books) {
+      this.booksResult.push(book);
+    }
+  }
+  searchName($event: any) {
+    let searchName = $event.target.value;
+    this.booksResult = [];
+    if(searchName === '') {
+      for(let book of this.books) {
+        this.booksResult.push(book);
+      }
+    }else {
+      for (let book of this.books) {
+        if (book.name.indexOf(searchName) !== -1) {
+          this.booksResult.push(book);
+        }
+      }
+    }
+  }
+}
